9f3b20df3e09b44efa6836cea8a15b2a7874df6c
Abdulquddus-Nuhu <abdulquddusnuhu@gmail.com>
2023-02-08 16:57:22 +0100

    configure MySQL db using pomelo.Mysql library

---
+++ src/BackgroundJob/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BackgroundJob.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackgroundJob.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly DbContextOptions<AppDbContext> _options;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+            _options = options;
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
+
