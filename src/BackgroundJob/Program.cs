2369bb0fc3b84b0509ec6d53fbcd4f8514117566
Abdulquddus-Nuhu <abdulquddusnuhu@gmail.com>
2023-02-09 11:20:21 +0100

    implemented recurring job

--- src/BackgroundJob/Program.cs
+++ src/BackgroundJob/Program.cs
@@ -1,16 +1,21 @@
-﻿using BackgroundJob.Data;
+﻿using System.Net;
+using BackgroundJob;
+using BackgroundJob.Data;
+using Hangfire;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>( options =>
-{
-    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),  ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")));
-});
+//Registration of services
+builder.Services.RegisterService(builder.Environment, builder.Configuration);
 
 var app = builder.Build();
 
+app.UseHangfireDashboard();
+
 app.MapGet("/", () => "Hello World!");
 
-app.Run();
+RecurringJob.AddOrUpdate(() => Console.WriteLine("Hello bro"), Cron.Minutely);
 
+app.Run();
+//HttpStatusCode.Created;
---
+++ src/BackgroundJob/RegisterServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BackgroundJob.Data;
+using Hangfire;
+using Hangfire.MySql;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackgroundJob
+{
+    public static class RegisterServices
+    {
+        public static IServiceCollection RegisterService(this IServiceCollection services, IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            //Logging
+            services.AddLogging();
+
+            //Database
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
+
+            //Hangfire
+            services.AddHangfire(x => x.UseStorage(new MySqlStorage(
+                    connectionString,
+                    new MySqlStorageOptions
+                    {
+                        QueuePollInterval = TimeSpan.FromSeconds(10),
+                        JobExpirationCheckInterval = TimeSpan.FromHours(1),
+                        CountersAggregateInterval = TimeSpan.FromMinutes(5),
+                        PrepareSchemaIfNecessary = true,
+                        DashboardJobListLimit = 25000,
+                        TransactionTimeout = TimeSpan.FromMinutes(1),
+                        TablesPrefix = "Hangfire",
+                    })));
+            services.AddHangfireServer();
+
+            return services;
+        }
+    }
+}
